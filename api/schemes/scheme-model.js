32cfcc85ee867aaca4ef7bb5d73de24f2f9f78cb
dwilliams304 <d.williams0434@gmail.com>
2023-10-05 15:08:10 -0700

    Finished

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -144,7 +144,7 @@ async function findSteps(scheme_id) { // EXERCISE C
       .where('st.scheme_id', scheme_id)
       .orderBy('st.step_number')
 
-  if (!rows[0].step_id) return [];
+  if (!rows[0]) return []
   return rows;
 
 }
@@ -155,7 +155,7 @@ function add(scheme) { // EXERCISE D
   */
  return db('schemes').insert(scheme)
   .then(([id]) => {
-    return db('schemes').where('id', id)
+    return db('schemes').where('scheme_id', id).first();
   })
 }
 
@@ -165,6 +165,16 @@ function addStep(scheme_id, step) { // EXERCISE E
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+ return db('steps').insert({
+    ...step,
+    scheme_id
+ }).then(() => {
+    return db('steps as st')
+    .join('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
+    .select('step_id', 'step_number', 'instructions', 'scheme_name')
+    .orderBy('step_number')
+    .where('sc.scheme_id', scheme_id)
+ })
 }
 
 module.exports = {
